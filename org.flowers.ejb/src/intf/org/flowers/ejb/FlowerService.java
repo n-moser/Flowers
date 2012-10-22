f6528075419ee1300ed4a25dbbc16cad81c1957e
Nicolas Moser <nico@moser-home.de>
2012-10-22 22:46:49 +0200

    EJB Service added

--- org.flowers.ejb/src/intf/org/flowers/ejb/FlowerService.java
+++ org.flowers.ejb/src/intf/org/flowers/ejb/FlowerService.java
@@ -2,11 +2,8 @@ package org.flowers.ejb;
 
 import java.util.List;
 
-import javax.ejb.Stateless;
-
 import org.flowers.entity.Flower;
 
-@Stateless
 public interface FlowerService {
 
 	List<Flower> loadFlowers();
